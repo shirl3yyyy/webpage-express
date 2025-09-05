3d21346f1c47cce1cf058d3ada0f7b52cb35a592
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-05 13:24:53 -0700

    addded server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const workingHours = require('./workingHours');
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(workingHours);
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello Express working!');
+    res.render('home');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+app.get('/contact', (req,res) =>{
+    res.render('contact');
 });
 
 const PORT = process.env.PORT || 3000;
